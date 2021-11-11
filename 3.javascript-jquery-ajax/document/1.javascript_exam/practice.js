792530c3535e971fa714efb6753b8883fe8cf68b
masatonara83 <masato.nara@rakus-partners.co.jp>
2021-11-12 08:46:28 +0900

    avaScript課題完了

--- 3.javascript-jquery-ajax/document/1.javascript_exam/practice.js
+++ 3.javascript-jquery-ajax/document/1.javascript_exam/practice.js
@@ -1,16 +1,31 @@
 console.log("演習2")
 {
-
+console.log('nara')
 }
 
 console.log("演習3")
 {
-
+	let lastName = '奈良';
+	let firstName = '将人';
+	console.log(lastName);
+	console.log(firstName);
+	
 }
 
 console.log("演習4")
 {
-
+	let age = 65;
+	if(age >= 0 && age < 5){
+		console.log('幼年期');
+	} else if(age < 15) {
+		console.log('少年期');
+	}else if(age < 25){
+		console.log('青年期');
+	}else if(age < 45 && age <= 64){
+		console.log('中年期');
+	} else {
+		console.log('更年期');
+	}
 }
 
 console.log("演習4-2")
@@ -20,25 +35,72 @@ console.log("演習4-2")
 
 console.log("演習5")
 {
-	
+	let num = 0;
+	for(let i = 1; i <= 10; i++){
+		num += i;
+	}
+	console.log(num);
 }
 
 console.log("演習5-2")
 {
+	let nums = "";
 	
+	for(let i = 9; i > 0; i--){
+		for(let j = 9; j > 0; j--){
+			let num = i * j;
+			if(num < 10){
+				nums += ' ' + num + ' ';
+			}else {
+				nums += num + ' ';
+			}
+		}
+		nums += "\n";
+	}
+	
+	console.log(nums);
 }
 
 console.log("演習6")
 {
+	let number = "";
+	let nums = [1,2,3,4,5,6,7,8,9,10];
+	for(let i = 0; i < nums.length; i++){
+		number += nums[i] + " ";
+	}
+	console.log(number);
 	
+	number = "";
+	for(let num of nums){
+		number += num + " ";
+	}
+	console.log(number);
 }
 
 console.log("演習6-2")
 {
-
+	let kisu = "";
+	let nums = [1,2,3,4,5,6,7,8,9,10];
+	for(let num of nums){
+		if(num % 2 == 1){
+			kisu += num + " ";
+		}
+	}
+	
+	console.log(kisu);
 }
 
 console.log("演習7")
 {
-
+	let country = {
+		'日本':'東京',
+		'スリランカ':'スリジャヤワルダナプラコッテ',
+		'タイ':'クルンテープ・マハーナコーン・アモーンラッタナコーシン・マヒンタラーユッタヤー・マハーディロック・ポップ・ノッパラット・ラーチャタニーブリーロム・ウドムラーチャニウェートマハーサターン・アモーンピマーン・アワターンサティット・サッカタッティヤウィサヌカムプラシット'
+	};
+	console.log(country['日本']);
+	console.log(country['タイ']);
+	country['タイ'] = 'バンコク';
+	console.log(country['タイ']);
+	
+	
 }
--- 3.javascript-jquery-ajax/document/1.javascript_exam/practice2.js
+++ 3.javascript-jquery-ajax/document/1.javascript_exam/practice2.js
@@ -1,29 +1,84 @@
 console.log("演習8")
 {
-
+	function printName(){
+		console.log('奈良将人');
+	}
+	printName();
 }
 
 console.log("演習8-2")
 {
-
+	function add(a,b){
+		return a + b;
+	}
+	function sub(a,b){
+		return a - b;
+	}
+	function multi(a,b){
+		return a * b;
+	}
+	function div(a,b){
+		return a / b;
+	}
+	console.log(add(5,3));
+	console.log(sub(5,3));
+	console.log(multi(5,3));
+	console.log(div(5,3));
 }
 
 console.log("演習9")
 {
-
+	let printNameVariable = {
+		name: function(){console.log('nara');}
+	}
+	printNameVariable.name();
 }
 
 console.log("演習9-2")
 {
-
+	let addVariable = function(a,b){
+		return a + b;
+	}
+	let subVariable = function(a,b){
+		return a - b;
+	}
+	let multiVariable = function(a,b){
+		return a * b;
+	}
+	let divVariable = function(a,b){
+		return a / b;
+	}
+	
+	console.log(addVariable(5,3));
+	console.log(subVariable(5,3));
+	console.log(multiVariable(5,3));
+	console.log(divVariable(5,3));
 }
 
 console.log("演習10")
 {
-
+	var globalVal = 10;
+	function func1(){
+		var localVal = 20;
+		console.log('globalVal: ' + globalVal);
+		console.log('localVal: ' + localVal);
+	}
+	
+	func1();
+	console.log('globalVal: ' + globalVal);
+	//console.log('localVal' + localVal);
 }
 
 console.log("演習11")
 {
-
+	func1();
+	function func1(){
+		 x = 6;
+		if(true){
+			let x = 5;
+			
+		}
+		console.log(x);
+	}
+	func1();
 }
